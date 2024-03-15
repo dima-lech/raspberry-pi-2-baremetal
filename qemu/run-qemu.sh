56c7ef26b46537356b5ed77066e50decfb06801f
dima-lech <dima.lech@gmail.com>
2024-03-15 15:14:14 +0200

    updated qemu script: search for machine, print command

--- qemu/run-qemu.sh
+++ qemu/run-qemu.sh
@@ -1,5 +1,11 @@
+#/bin/bash
+
+set -v
+
+machine=`qemu-system-arm -machine help | grep raspi2 | cut -d' ' -f1`
+
 qemu-system-arm \
-    -M raspi2 \
+    -M $machine \
     -dtb qemu/bcm2836-rpi-2-b.dtb \
     -kernel out/baremetal.bin \
     -m 1G \
